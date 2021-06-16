ccc85fa3305f24b3e1312f2fc079a08b428b750a
Ryan Smith <smith.a.ryan.98@gmail.com>
2021-06-16 02:36:36 -0400

    refactor: reduce string copying

--- tdameritrade_api/utils/urls.py
+++ tdameritrade_api/utils/urls.py
@@ -9,6 +9,7 @@
 # -Base URLs
 base = "https://api.tdameritrade.com/v1/"
 # -Authorization Endpoints
+# [POST]
 auth_oauth = "oauth2/token"  # [POST]
 # -Account Endpoints
 account_get_by_id = "accounts/{}"  # [GET]
@@ -17,31 +18,28 @@ account_get_by_ids = "accounts"    # [GET]
 transaction_get_by_id = "accounts/{}/transactions/{}"    # [GET]
 transaction_get_by_account = "accounts/{}/transactions"  # [GET]
 # -Account: Order Endpoints
-order_create = "accounts/{}/orders"          # [POST]
-order_replace = "accounts/{}/orders/{}"      # [PUT]
-order_delete = "accounts/{}/orders/{}"       # [DELETE]
-order_list = "orders"                        # [GET]
-order_get_by_id = "accounts/{}/orders/{}"    # [GET]
-order_get_by_account = "accounts/{}/orders"  # [GET]
+order_list = "orders"  # [GET]
+# [POST] | [GET]
+order_create = order_get_by_account = "accounts/{}/orders"
+# [PUT] | [DELETE] | [GET]
+order_replace = order_delete = order_get_by_id = "accounts/{}/orders/{}"
 # -Account: Saved Order Endpoints
-so_create = "accounts/{}/savedorders"        # [POST]
-so_replace = "accounts/{}/savedorders/{}"    # [PUT]
-so_delete = "accounts/{}/savedorders/{}"     # [DELETE]
-so_get_by_id = "accounts/{}/savedorders/{}"  # [GET]
-so_get_by_account = "{}/savedorders"         # [GET]
+# [PUT] | [DELETE]  | [GET]
+so_replace = so_delete = so_get_by_id = "accounts/{}/watchlists/{}"
+# [POST] | [GET]
+so_create = so_get_by_account = "accounts/{}/savedorders"
 # -Account: Watchlist Endpoints
-watchlist_create = "accounts/{}/watchlists"          # [POST]
-watchlist_update = "accounts/{}/watchlists/{}"       # [PATCH]
-watchlist_replace = "accounts/{}/watchlists/{}"      # [PUT]
-watchlist_delete = "accounts/{}/watchlists/{}"       # [DELETE]
-watchlist_list = "accounts/watchlists"               # [GET]
-watchlist_get_by_id = "accounts/{}/watchlists/{}"    # [GET]
-watchlist_get_by_account = "accounts/{}/watchlists"  # [GET]
+watchlist_list = "accounts/watchlists"  # [GET]
+# [POST] | [GET]
+watchlist_create = watchlist_get_by_account = "accounts/{}/watchlists"
+# [PATCH] | [PUT] | [DELETE] | [GET]
+watchlist_update = watchlist_replace = watchlist_delete = watchlist_get_by_id =\
+    "accounts/{}/watchlists/{}"
 # -Account: User Info & Preference Endpoints
 principals_list = "userprincipals"                             # [GET]
 subscription_list = "userprincipals/streamersubscriptionkeys"  # [GET]
-preference_update = "accounts/{}/preferences"                  # [PUT]
-preference_get_by_id = "accounts/{}/preferences"               # [GET]
+# [PUT] | [GET]
+preference_update = preference_get_by_id = "accounts/{}/preferences"
 # -Instrument Endpoints
 instrument_list = "instruments"          # [GET]
 instrument_get_by_id = "instruments/{}"  # [GET]
