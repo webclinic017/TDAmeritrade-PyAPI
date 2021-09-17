de8d1221440cb144b0e01d989473c920ce39c623
Ryan Smith <smith.a.ryan.98@gmail.com>
2021-09-17 18:49:50 -0400

    refactor: library module

--- tdameritrade_api/__init__.py
+++ tdameritrade_api/__init__.py
@@ -1,17 +1,18 @@
-##-----------------------------##
-## TDAmeritrade PyAPI          ##
-## Written By: Ryan Smith      ##
-##-----------------------------##
+#!/usr/bin/python
+##-------------------------------##
+## TDAmeritrade PyAPI            ##
+## Written By: Ryan Smith        ##
+##-------------------------------##
 
 ## Imports
-from .profile import Profile
+from .profile.session import Session, WebSocket
 
 ## Constants
 __author__ = "Ryan Smith"
 __project__ = "TDAmeritrade PyAPI"
 __version__ = (1, 0, 0)
 __all__ = [
-    Profile
+    Session, WebSocket
 ]
 
 
