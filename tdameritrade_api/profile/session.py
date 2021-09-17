4007343434c9e8854cfc2076404d19943a8ec293
Ryan Smith <smith.a.ryan.98@gmail.com>
2021-09-17 14:11:33 -0400

    refactor(Session): new implementation

---
+++ tdameritrade_api/profile/session.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+##-------------------------------##
+## TDAmeritrade PyAPI            ##
+## Written By: Ryan Smith        ##
+##-------------------------------##
+## Session + WebSocket Classes   ##
+##-------------------------------##
+
+## Imports
+from __future__ import annotations
+
+## Variables
+
+
+## Functions
+
+
+## Classes
+class Session:
+    """TDAmeritrade Session"""
+
+    # -Constructor
+    def __init__(self) -> Session:
+        pass
+
+    # -Dunder Methods
+
+    # -Instance Methods
+
+    # -Class Methods
+
+    # -Static Methods
+
+    # -Properties
+
+    # -Class Properties
+
+    # -Sub-Classes
+
+
+class WebSocket:
+    """TDAmeritrade WebSocket"""
+
+    # -Constructor
+    def __init__(self) -> WebSocket:
+        pass
+
+    # -Dunder Methods
+
+    # -Instance Methods
+
+    # -Class Methods
+
+    # -Static Methods
+
+    # -Properties
+
+    # -Class Properties
+
+    # -Sub-Classes
