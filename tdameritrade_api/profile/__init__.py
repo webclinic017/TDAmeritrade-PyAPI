da5e2b5e093806321e07ea9cac86e394eb344981
Ryan Smith <smith.a.ryan.98@gmail.com>
2021-09-22 15:31:11 -0400

    add(Profile): me method

--- tdameritrade_api/profile/__init__.py
+++ tdameritrade_api/profile/__init__.py
@@ -8,7 +8,9 @@
 
 ## Imports
 from __future__ import annotations
+
 from .session import Session, WebSocket
+from ..utils import urls
 
 ## Constants
 __all__ = [
@@ -23,3 +25,22 @@ class Profile:
     # -Constructor
     def __init__(self, session: Session) -> Profile:
         self._session: Session = session
+
+    # -Instance Methods
+    async def me(
+        self, *, preferences: bool = True, surrogates: bool = True,
+        streamer_info: bool = True, streamer_keys: bool = True
+    ) -> dict[str, str]:
+        '''Profile details'''
+        fields = []
+        if preferences:
+            fields.append("preferences")
+        if surrogates:
+            fields.append("surrogateIds")
+        if streamer_info:
+            fields.append("streamerConnectionInfo")
+        if streamer_keys:
+            fields.append("streamerSubscriptionKeys")
+        return await self._session.get(urls.user_principals, params={
+            'fields': ','.join(fields)
+        })
--- tdameritrade_api/utils/urls.py
+++ tdameritrade_api/utils/urls.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 ##-----------------------------##
 ## TDAmeritrade PyAPI          ##
 ## Written By: Ryan Smith      ##
@@ -6,74 +7,21 @@
 ##-----------------------------##
 
 ## Imports
-from typing import Optional
+from __future__ import annotations
 
 ## Constants
 # -Base URLs
 base = "https://api.tdameritrade.com/v1/"
-base_account = base + "accounts"
-base_market = base + "marketdata/"
-# -Authorization Endpoints
+# -Authorization
 auth_oauth = base + "oauth2/token"
-# -Market Data: Option Endpoints
-option_list = base_market + "chains"
+# -Account
+base_account = base + "accounts"
+# -User Principals
+user_principals = base + "userprincipals"
+user_subscription = user_principals + "/streamersubscriptionkeys"
 
 
 ## Functions
-def get_account_endpoint(_id: Optional[int] = None, preferences: bool = False) -> str:
-    """Returns the account endpoint url depending on ID"""
-    return (base_account + (f"/{_id}" if _id else "")
-           + ("/preferences" if preferences else ""))
-
-
-def get_principals_endpoint(subscription: bool = False) -> str:
-    """Returns the principals endpoint url depending on subscription bool"""
-    return (base + "userprincipals"
-           + ("/streamersubscriptionkeys" if subscription else ""))
-
-
-def get_transaction_endpoint(id1: int, id2: Optional[int] = None) -> str:
-    """Returns the transaction endpoint url depending on IDs"""
-    return base_account + f"/{id1}/transactions" + (f"/{id2}" if id2 else "")
-
-
-def get_watchlist_endpoint(id1: Optional[int] = None, id2: Optional[int] = None) -> str:
-    """Returns the watchlist endpoint url depending on IDs"""
-    return (base_account + (f"/{id1}" if id1 else "")
-           + "/watchlists" + (f"/{id2}" if id2 else ""))
-
-
-def get_orders_endpoint(id1: Optional[int] = None, id2: Optional[int] = None) -> str:
-    """Returns the orders endpoint url depending on IDs"""
-    return ((base_account + f"/{id1}/" if id1 else base)
-           + "orders" + (f"/{id2}" if id2 else ""))
-
-
-def get_saved_orders_endpoint(id1: int, id2: Optional[int] = None) -> str:
-    """Returns the saved orders endpoint url depending on IDs"""
-    return base_account + f"/{id1}/savedorders" + (f"/{id2}" if id2 else "")
-
-
-def get_instrument_endpoint(_id: Optional[str] = None) -> str:
-    """Returns the instrument endpoint url depending on ID"""
-    return base + "instruments" + (f"/{_id}" if _id else "")
-
-
-def get_market_hours_endpoint(_id: Optional[str] = None) -> str:
-    """Returns the market hours endpoint url depending on ID"""
-    return base_market + (f"{_id}/" if _id else "") + "hours"
-
-
-def get_market_quote_endpoint(
-    _id: Optional[str] = None, *,
-    historical: bool = False, mover: bool = False
-) -> str:
-    """Returns the market quotes, historicals, or movers
-    endpoint url depending on ID and historical/mover bool"""
-    _str = base_market + (f"{_id}/" if _id else "")
-    if historical:
-        return _str + "pricehistory"
-    elif mover:
-        return _str + "movers"
-    else:
-        return _str + "quotes"
+def account_id(id_: int, preferences: bool = False) -> str:
+    """Returns account endpoint based on parameters"""
+    return f"{base_account}/{str(id_)}" + ("/preferences" if preferences else "")
