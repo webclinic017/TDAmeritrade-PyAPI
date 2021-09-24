0a8556159c54a4703616b6f2709effa320a34bbe
Ryan Smith <smith.a.ryan.98@gmail.com>
2021-09-23 20:06:34 -0400

    tests: Session + WebSocket

---
+++ tests/test_session.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+##-------------------------------##
+## TDAmeritrade PyAPI            ##
+## Written By: Ryan Smith        ##
+##-------------------------------##
+## Session Class: Testing        ##
+##-------------------------------##
+
+## Imports
+import unittest
+
+from tdameritrade_api.session import Session
+
+
+## Classes
+class Test_Session(unittest.IsolatedAsyncioTestCase):
+
+    # -Instance Methods
+    async def asyncSetUp(self):
+        pass
+
+    async def asyncTearDown(self):
+        pass
---
+++ tests/test_websocket.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+##-------------------------------##
+## TDAmeritrade PyAPI            ##
+## Written By: Ryan Smith        ##
+##-------------------------------##
+## WebSocket Class: Testing      ##
+##-------------------------------##
+
+## Imports
+import unittest
+
+from tdameritrade_api.websocket import WebSocket
+
+
+## Classes
+class Test_WebSocket(unittest.IsolatedAsyncioTestCase):
+
+    # -Instance Methods
+    async def asyncSetUp(self):
+        pass
+
+    async def asyncTearDown(self):
+        pass
